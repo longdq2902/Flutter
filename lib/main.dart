12ce3c5c5de743d07b014d3315f9ce676d850178
LongDQ <longdq@vnpt-technology.vn>
2020-04-14 23:11:04 +0700

    Image from URL

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-
+//import 'package:english_words/english_words.dart';
+import 'urlImage.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,62 +9,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: RandomEnglishWords(),
+      home: new URLImage(),
     );
   }
 }
 
-class RandomEnglishWords extends StatefulWidget {
-  @override
-  RandomEnglishWordsState createState() => RandomEnglishWordsState();
-}
 
-class RandomEnglishWordsState extends State<RandomEnglishWords> {
-  final _words = <WordPair>[];
-  final _selectedWord = new Set<WordPair>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("ListView Demo"),
-      ),
-      body: new ListView.builder(itemBuilder: (context,index){
-        //index = 0,1,2,3 là row của listview
-        if(index >= _words.length){
-          _words.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_words[index], index);
-      }),
-    );
-  }
-
-  Widget _buildRow (WordPair wordPair, int index) {
-    // this function will build a row of listview
-    final ischecked = _selectedWord.contains(wordPair);
-    return ListTile(
-      leading: new Icon(ischecked ? Icons.check_box : Icons.check_box_outline_blank, color: Colors.blue,),
-      title: new Text(
-        wordPair.asUpperCase,
-        style: new TextStyle(fontSize: 15.0, color: Colors.blue),
-      ),
-      onTap: (){
-          setState(() {
-            if(ischecked) {
-              _selectedWord.remove(wordPair);
-            }else {
-              _selectedWord.add(wordPair);
-            }
-          });
-      },
-    );
-  }
-
-}
 
 
 
