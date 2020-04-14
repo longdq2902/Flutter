412d1b6131bafe98f420a920bfe396b5d893c9cb
LongDQ <longdq@vnpt-technology.vn>
2020-04-14 10:51:32 +0700

    Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-
 
 void main() => runApp(MyApp());
 
@@ -26,6 +26,23 @@ class RandomEnglishWords extends StatefulWidget {
 class RandomEnglishWordsState extends State<RandomEnglishWords> {
   final _words = <WordPair>[];
   final _selectedWord = new Set<WordPair>();
+  final _txtTitle = new Container(
+    padding: const EdgeInsets.all(10.0),
+    child: new Row(
+      children: <Widget>[
+        new Expanded(child:
+            new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text("Dòng số 1 đây nhé", style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+                new Text("Dòng số 2 đây nhé",style: new TextStyle(fontSize: 16.0))
+              ],
+            ),
+        ),
+        new Text("Dòng số 2 đây nhé",style: new TextStyle(fontSize: 16.0))
+      ],
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -33,38 +50,36 @@ class RandomEnglishWordsState extends State<RandomEnglishWords> {
       appBar: AppBar(
         title: Text("ListView Demo"),
       ),
-      body: new ListView.builder(itemBuilder: (context,index){
-        //index = 0,1,2,3 là row của listview
-        if(index >= _words.length){
-          _words.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_words[index], index);
-      }),
+      body: new ListView(
+        children: <Widget>[
+          new Image.asset('images/tutorialChannel.png', fit: BoxFit.cover),
+          _txtTitle,
+        ],
+      ),
     );
   }
 
-  Widget _buildRow (WordPair wordPair, int index) {
+  Widget _buildRow(WordPair wordPair, int index) {
     // this function will build a row of listview
     final ischecked = _selectedWord.contains(wordPair);
     return ListTile(
-      leading: new Icon(ischecked ? Icons.check_box : Icons.check_box_outline_blank, color: Colors.blue,),
+      leading: new Icon(
+        ischecked ? Icons.check_box : Icons.check_box_outline_blank,
+        color: Colors.blue,
+      ),
       title: new Text(
         wordPair.asUpperCase,
         style: new TextStyle(fontSize: 15.0, color: Colors.blue),
       ),
-      onTap: (){
-          setState(() {
-            if(ischecked) {
-              _selectedWord.remove(wordPair);
-            }else {
-              _selectedWord.add(wordPair);
-            }
-          });
+      onTap: () {
+        setState(() {
+          if (ischecked) {
+            _selectedWord.remove(wordPair);
+          } else {
+            _selectedWord.add(wordPair);
+          }
+        });
       },
     );
   }
-
 }
-
-
-
